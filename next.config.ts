ad91934d1697cecb4ac067cfed78c508edbf79d2
Piyush Kumar <piyukum4@cisco.com>
2026-02-05 18:35:56 +0530

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/blog',
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  /* config options here */
 };
 
 export default nextConfig;
