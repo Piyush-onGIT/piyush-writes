1311d48e6d9c87d8bf69ec187de6d75ac9ee68d1
Piyush Kumar <piyukum4@cisco.com>
2026-02-05 19:19:16 +0530

    deploy

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export async function generateStaticParams() {
   }));
 }
 
-// export const dynamicParams = false;
+export const dynamicParams = false;
 
 export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
