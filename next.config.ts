fcf9cd0d073b2eedf27db85e004442b8eedfcd9e
Piyush Kumar <60540783+Piyush-onGIT@users.noreply.github.com>
2026-02-07 16:27:57 +0530

    Add basePath for production configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = process.env.NODE_ENV === "production" ? {
   output: "export",
   assetPrefix: '/piyush-writes',
+  basePath: '/piyush-writes',
   images: {
     unoptimized: true,
   },
