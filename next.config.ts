8d68c7775162293028c5d1beddd73f51c6699be0
Piyush <thisissahupiyush@gmail.com>
2026-02-07 17:05:44 +0530

    image path changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = { 
-  assetPrefix: 'https://piyush-ongit.github.io/piyush-writes',
+const nextConfig: NextConfig = {
+  // assetPrefix: 'https://piyush-ongit.github.io/piyush-writes',
+  basePath: '/piyush-writes',
 }
 
 export default nextConfig;
