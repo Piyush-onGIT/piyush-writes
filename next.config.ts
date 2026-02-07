65800831b2725145c2652cbfd1604c6fbdf5bb55
Piyush <thisissahupiyush@gmail.com>
2026-02-07 16:35:11 +0530

    added asset prefix in next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = process.env.NODE_ENV === "production" ? {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-} :
-{ 
+const nextConfig: NextConfig = { 
   assetPrefix: '/piyush-writes',
 }
 
